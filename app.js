ab2b63921030075f5ad1cbcf6c9378dd47fc2ba3
Johnny Meza <xlpacman805@gmail.com>
2020-06-07 18:05:17 -0700

    making a working rest api for now

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const fs = require('fs');
-const bmwdata = require('./bmw-response-93010.json');
-const Series3Offers = bmwdata.offers.series3;
+const express = require('express')
+const axios = require('axios').default;
+const cors = require('cors')
+const app = express()
+const port = 4000
 
-let data = JSON.stringify(Series3Offers[0]);
+app.use(cors())
 
-fs.writeFile('output.json', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+app.get('/', (req, res) => {
+  axios.get('https://www.bmwusa.com/bin/services/offers?zipCode=93010')
+    .then(result => res.json(result.data.offers))
+    .catch(err => res.json({error: err.toString()}))
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
